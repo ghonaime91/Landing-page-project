23ae56d6e8e9a7e983f52369edbc0261f0b43e85
Ahmed Ghonaime <hoznabady@gmail.com>
2022-03-20 21:43:48 +0200

    udacity nanodegree

---
+++ README.md
@@ -0,0 +1,15 @@
+# Landing Page Project
+
+The first project in the web development professional track in the FWD scholarship, 
+And nanodegree from Udacity 
+
+## What has been done on this project:
+
+1- Adding a Navigation Bar dynamically by Just Native JavaScript and DOM .
+2- Add functionality to distinguish the section in view.
+3- Add the functionality to scroll to sections.
+4- Add an active state to navigation items when a section is in the viewport.
+5- It was taken into account that the code is high-performance.
+
+
+
---
+++ js/app.js
@@ -0,0 +1,131 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+
+/**
+ * Define Global Variables
+ * 
+*/
+const allSections = document.querySelectorAll("section");
+const ul = document.getElementById("navbar__list");
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+//function to check if the section in viewport or not
+function inViewPort(el){
+    flag = false;
+    if(el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().top <= 450 )
+    {
+        flag = true;
+    } 
+    return flag;
+}
+
+// function to add and remove a specific class
+function controlClasses(flag,element,cNamce) {
+    switch(flag) {
+        case 1:
+            element.classList.add(cNamce);
+            break;
+        case 2:
+            element.classList.remove(cNamce);
+            break;    
+    }
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+function navBuilder() {
+    //using of document fragment when appending li to improve the performance
+    const frag = document.createDocumentFragment();
+        for(let i=0;i<allSections.length;i++)
+    {
+        const id = allSections[i].getAttribute('id');
+        const li = document.createElement("li");
+        const anchor = document.createElement("a");
+        anchor.textContent = allSections[i].getAttribute('data-nav');
+        anchor.setAttribute('href',"#"+id);
+        anchor.setAttribute('id',"#"+id);
+        anchor.className = "menu__link";
+        li.appendChild(anchor);
+        frag.appendChild(li);
+    }
+    ul.appendChild(frag);
+}
+
+//invoke nav builder function
+navBuilder(); 
+
+//all linkes
+
+const links= document.querySelectorAll("li a.menu__link");
+
+
+// Add class 'active' to section when near top of viewport
+
+function addActiveClass() {
+    for(let i=0; i<allSections.length;i++) {
+        if(inViewPort(allSections[i]) &&
+         links[i].textContent === allSections[i].getAttribute("data-nav"))
+        {
+            controlClasses(1,links[i],"your-active-class");
+            controlClasses(1,links[i],"your-active-link");         
+            controlClasses(1,allSections[i],"your-active-class");
+            controlClasses(1,allSections[i],"your-active-link");
+         
+        } else {
+            controlClasses(2,links[i],"your-active-class");
+            controlClasses(2,links[i],"your-active-link");            
+            controlClasses(2,allSections[i],"your-active-class");
+            controlClasses(2,allSections[i],"your-active-link");
+            
+        }
+    }
+}
+
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+
+
+// Smooth Scroll
+links.forEach(link=>{
+    link.addEventListener('click',(evt)=>{
+        evt.preventDefault();
+        const id = evt.target.id;
+        const section = document.querySelector(id);
+        section.scrollIntoView({behavior:"smooth"})
+    })
+})
+// Set sections as active
+document.addEventListener("scroll",addActiveClass);
+
+
